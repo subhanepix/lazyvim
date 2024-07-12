ac2c6a8d7ece22352c18bf89022f05d62df5f001
subhanepix <94199944+subhanepix@users.noreply.github.com>
2024-07-12 18:37:20 -0500

    added luasnip and snippets

---
+++ lua/config/snippets/all.lua
@@ -0,0 +1,26 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+ls.add_snippets("lua", {
+  s("hello", {
+    t('print("hello world")'),
+  }),
+})
+
+ls.add_snippets("html", {
+  s("skelly", {
+    t(
+      "<!DOCTYPE html>",
+      '<html lang="en">',
+      "<head>",
+      '  <meta charset="UTF-8">',
+      "  <title></title>",
+      "</head>",
+      "<body>",
+      "</body>",
+      "</html>"
+    ),
+  }),
+})
