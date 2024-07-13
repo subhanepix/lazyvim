a9259816ed359cca270d41bfbb9fc9ea7baa0000
subhanepix <94199944+subhanepix@users.noreply.github.com>
2024-07-12 19:15:42 -0500

    i think i fixed luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,11 +1,14 @@
 return {
   {
-    "l3mon4d3/luasnip",
+    "L3MON4D3/LuaSnip",
     dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_lua").lazy_load({ paths = { "~/.config/nvim/lua/config/snippets" } })
-      end,
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_lua").lazy_load({ paths = "./lua/config/snippets" })
+        end,
+      },
     },
   },
 }
